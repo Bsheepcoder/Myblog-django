fb82c44b77ecdf790916a997f079cc2c404d1156
qxd <710418455@qq.com>
2023-03-05 21:02:32 +0800

    用户登录改造

--- userprofile/utils/imgCode.py
+++ userprofile/utils/imgCode.py
@@ -8,7 +8,7 @@ import random
 import string
 
 
-def check_code(width=120, height=30, char_length=5, font_file='/userprofile/utils/Monaco-1.ttf', font_size=28):
+def check_code(width=120, height=30, char_length=5, font_file='./userprofile/utils/Monaco-1.ttf', font_size=28):
     # 创建图片
     img = Image.new(mode='RGB', size=(width, height), color=(193, 193, 193))
     draw = ImageDraw.Draw(img, mode='RGB')
