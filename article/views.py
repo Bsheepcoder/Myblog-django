01c7bca77d51727f8dbbbc066c25accabfb9f4cb
qxd <710418455@qq.com>
2023-03-15 17:32:42 +0800

    解决js的渲染问题

--- article/views.py
+++ article/views.py
@@ -66,12 +66,12 @@ def article_list(request):
 # 找到markdown中的frontmatter属性
 
 id_all = None
+
+
 # 文章详情
 def article_detail(request, id):
-
     global id_all
     id_all = id
-
 
     # 取出相应的文章
     article = ArticlePost.objects.get(id=id)
@@ -86,14 +86,12 @@ def article_detail(request, id):
                'body': json.dumps(body),
                'id': id,
                }
-    print(type(body))
     # 载入模板，并返回context对象
     return render(request, 'article/detail.html', context)
 
 
 def get_article_body(request):
     article = ArticlePost.objects.get(id=id_all)
-    print(1111)
     result = {
         "status": True,
         "data": {
