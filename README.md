289f492cfbf2a311dc82f233156f93f77df7a0a7
qxd <710418455@qq.com>
2023-03-21 15:19:57 +0800

    mysql安全处理

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# mybolg-django-bootstrap4
+# myblog-django-bootstrap4
 
 
 ##  version ：0.0.4 2023.3.3
 - based on django and bootstrap4  0.0.4
 ##  version ：0.0.5 2023.3.15
 - resovle issue about render the js by editor.md 
+- 遭遇了一些安全性问题，对项目的安全性做优化
--- blog_04/settings.py
+++ blog_04/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myblog',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'root',
+        'PASSWORD': 'Cqzod@123',
         'HOST': '47.115.218.160',
         'PORT': 3306,
     }
